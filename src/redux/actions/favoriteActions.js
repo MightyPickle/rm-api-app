77210a27727b56b5e2a0afa8f82f693b144159f7
U10 <uliaten10@gmail.com>
2022-10-12 18:50:37 +0300

    eslint fix

--- src/redux/actions/favoriteActions.js
+++ src/redux/actions/favoriteActions.js
@@ -2,4 +2,3 @@ import { DISLIKE_CHARACTER, LIKE_CHARACTER } from '../types/types';
 
 // eslint-disable-next-line import/prefer-default-export
 export const likeCharacterAC = (payload) => ({ type: LIKE_CHARACTER, payload });
-
