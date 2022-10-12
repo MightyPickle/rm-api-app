3efd2a835317c8f9295e1627d996cdf527bb109f
U10 <uliaten10@gmail.com>
2022-10-12 23:47:28 +0300

    local icons fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/rm-api-app">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
